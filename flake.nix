cbe7ea7f1f0d7566dd1598fe70a9924756f3a93f
Sumner Evans <me@sumnerevans.com>
2023-12-17 19:41:24 -0700

    flake.nix: cleanup
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -6,31 +6,20 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { system = system; };
-        in
-        rec {
-          packages.mineshspc = pkgs.buildGoModule rec {
-            pname = "mineshspc.com";
-            version = "unstable-2023-05-15";
-            src = self;
-            subPackages = [ "cmd/mineshspc" ];
-            vendorSha256 = "sha256-vEOzqEaH2v4B4CKCkSbkNBX26LJ/d4X13hQtDH/fi8g=";
-          };
-          packages.default = packages.mineshspc;
+    (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages.mineshspc = pkgs.buildGoModule {
+          pname = "mineshspc.com";
+          version = "unstable-2023-05-15";
+          src = self;
+          subPackages = [ "cmd/mineshspc" ];
+          vendorSha256 = "sha256-vEOzqEaH2v4B4CKCkSbkNBX26LJ/d4X13hQtDH/fi8g=";
+        };
+        packages.default = packages.mineshspc;
 
-          devShells = {
-            default = pkgs.mkShell {
-              packages = with pkgs; [
-                go_1_20
-                gopls
-                gotools
-                pre-commit
-              ];
-            };
-          };
-        }
-      ));
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ go_1_20 gopls gotools pre-commit ];
+        };
+      }));
 }
