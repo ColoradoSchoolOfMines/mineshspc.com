3227d0336f24bd6a62192894326ef2cfdc84f23d
Sumner Evans <me@sumnerevans.com>
2024-05-21 23:44:29 -0600

    deps: add templ
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- flake.nix
+++ flake.nix
@@ -3,23 +3,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    templ = {
+      url = "github:a-h/templ/v0.2.697";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, templ }:
     (flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays =
+            [ (final: prev: { inherit (templ.packages.${system}) templ; }) ];
+        };
       in rec {
         packages.mineshspc = pkgs.buildGo122Module {
           pname = "mineshspc.com";
           version = "unstable-2024-05-21";
           src = self;
           subPackages = [ "cmd/mineshspc" ];
-          vendorHash = "sha256-QjNH3O5/Hpu0OuVLUMN//f4d2j4wsDiUEfGLXH/Yf04=";
+          vendorHash = "sha256-qlDZkMlvzrRhE/GdEz4hIQgk4BI2YrvOLazAFqs8xuY=";
+
+          preBuild = ''
+            ${pkgs.templ}/bin/templ generate
+          '';
         };
         packages.default = packages.mineshspc;
 
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ go gopls gotools pre-commit ];
+          packages = with pkgs; [ go gopls gotools pre-commit pkgs.templ ];
         };
       }));
 }
