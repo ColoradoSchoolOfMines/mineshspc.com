96e8c65c5aa6d2062011f43917fe8ca93d16a2a5
Sumner Evans <sumner.evans@automattic.com>
2024-12-19 08:39:42 -0700

    nix: fix package
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- flake.nix
+++ flake.nix
@@ -9,12 +9,12 @@
     (flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in rec {
-        packages.mineshspc = pkgs.buildGo122Module {
+        packages.mineshspc = pkgs.buildGoModule {
           pname = "mineshspc.com";
           version = "unstable-2024-12-19";
           src = self;
           subPackages = [ "cmd/mineshspc" ];
-          vendorHash = "sha256-Uoj6gN2w5eI6YVhNwlOHZ8Kr4ljC18+DCplCtt+OUNM=";
+          vendorHash = "sha256-IjEtu9V+TEE3nPJmYcka0MEJmccJAqXod6kII+Ac0hY=";
         };
         packages.default = packages.mineshspc;
 
